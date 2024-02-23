3d80e591c9a78d1d4ac313d996799abbdeb20ce5
Mohammad Hasim Shaikh <hasims2001@gmail.com>
2024-02-23 08:06:03 +0530

    80%: add new pizza design

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,12 +1,14 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Dashboard } from "../Components/Dashboard/Dashboard.jsx";
 import { Login } from "../Components/Login/Login";
+import { AddNewPizza } from "../Components/AddNewPizza/AddNewPizza.jsx";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/addpizza" element={<AddNewPizza />} />
     </Routes>
   );
 };
